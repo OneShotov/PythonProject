564d4a02d4d46aa101e4007a49926f8096865fbe
OneShotov <head86086@gmail.com>
2025-07-25 23:09:40 +0300

    текст конекшон 😁

---
+++ main.py
@@ -0,0 +1,20 @@
+variable01 = 2
+variable02 = "Hello World"
+variable03 = 1.5
+variable04 = []
+variable05 = {}
+variable06 = tuple()
+variable07 = set ()
+variable08 = None
+
+print(type(variable01))
+print(type(variable02))
+print(type(variable03))
+print(type(variable04))
+print(type(variable05))
+print(type(variable06))
+print(type(variable07))
+print(type(variable08))
+
+
+AlalalLa_LA_LA
